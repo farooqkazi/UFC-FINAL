2972561b36b15ad66652de31ed41e898853e06ca
farooqkazi <mohammedfarooqkazi@gmail.com>
2017-05-24 21:56:20 +0530

    testing yaml

--- app/src/androidTest/java/com/example/kazi/testapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/kazi/testapplication/ApplicationTest.java
@@ -9,8 +9,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 /**
@@ -31,6 +35,15 @@ public class ApplicationTest {
     public void tearDown() {
     }
 
+    @Test
+    public void nav_background_image(){
+       onView(withId(R.id.navigation_view_header_background_image));
+     }
+
+    @Test
+    public void textTest(){
+        onView(withText("UFC UNOFFICIAL"));
+    }
 
 
 }
